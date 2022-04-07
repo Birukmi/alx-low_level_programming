db47e08fbdc5f488516ef858407b76782761a17c
biruk <you@example.com>
2022-04-07 14:42:24 -0700

    100-intel

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * main - entry point 
- * Return: 1 after execution
+ * Return: - 1 after execution
  */
 int main(void)
 {
-fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+fprintf(stderr,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 return (1);
 }

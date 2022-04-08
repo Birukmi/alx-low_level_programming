33435093d65ed0e1eab17c6e965a17d8d0020402
biruk <you@example.com>
2022-04-08 14:01:32 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main- entry point
+ *
+ * Return: 0 after execution
+ */
+int main(void)
+{
+	char alpha;
+	for(alpha = 'a'; alpha <= 'z'; z++)
+	{
+		putchar(alpha);
+	}
+	putchar('\n');
+return (0);
+}

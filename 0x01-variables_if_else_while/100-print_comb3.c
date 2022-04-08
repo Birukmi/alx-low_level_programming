ef2057a24e22e10a0822abd97019a8d917c2c5f3
biruk <you@example.com>
2022-04-08 16:47:37 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,12 +9,14 @@ int main(void)
 	int i;
 	int j;
 
-	for(i = 48; i < 57; i++)
+	for (i = 48; i < 57; i++)
 	{
 		for (j = 49; j <= 57; j++)
 		{
 			putchar(i);
 			putchar(j);
+			if (i == 56 && j === 57)
+				continue;
 			putchar(',');
 			putchar(' ');
 		}

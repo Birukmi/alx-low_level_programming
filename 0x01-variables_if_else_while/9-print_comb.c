f386dda31f49345bd6abf577e34dcf8d65073b4a
biruk <you@example.com>
2022-04-08 15:47:25 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main- entry point
+ *
+ * return: 0 after successful execution
+ */
+int main(void)
+{
+	int n;
+
+	for(n = 48; n <= 57, n++)
+	{
+		putchar(n);
+		putchar(,);
+		putchar( );
+	}
+
+	putchar('\n');
+	return (0);
+}

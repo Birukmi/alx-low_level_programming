460961415650fd85df457509a4497d7add36ad4a
biruk <you@example.com>
2022-04-08 14:54:50 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.>
+/**
+ * main- entry point
+ * Return: 0  for success execution
+ */
+int main(void)
+{
+	char alpha;
+
+	for(alpha = 'Z'; alpha >= 'A'; alpha--)
+	{
+		printf("%c". alpha);
+	}
+
+	printf("\n");
+	return (0);
+}

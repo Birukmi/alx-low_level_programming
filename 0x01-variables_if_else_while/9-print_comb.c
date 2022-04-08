7716d334dc459be5abdbcb024a0d588c105641bf
biruk <you@example.com>
2022-04-08 15:55:29 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -2,13 +2,13 @@
 /**
  * main- entry point
  *
- * return: 0 after successful execution
+ * Return: 0 after successful execution
  */
 int main(void)
 {
 	int n;
 
-	for(n = 48; n <= 57; n++)
+	for (n = 48; n <= 57; n++)
 	{
 		putchar(n);
 	if (n == 57)

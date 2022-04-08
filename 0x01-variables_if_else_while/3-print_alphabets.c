f29a74521554230e92a185c5464a2ce985884259
biruk <you@example.com>
2022-04-08 14:14:46 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main- entry point
+ *
+ * Return: 0 after Execution
+ */
+int main(void)
+{
+	char lower;
+	char upper;
+	
+	for (lower = 'a'; lower <= 'z'; lower++)
+	{
+		putchar(lower);
+	}
+	for(upper = 'A'; upper <= 'Z'; upper++)
+	{
+		putchar(upper);
+	}
+
+	putchar('\n');
+}

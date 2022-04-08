51d233550bc6053ea87648f4913dea13a8b7eaf3
biruk <you@example.com>
2022-04-08 15:24:15 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main- entry point'
+ *
+ * Return: 0 after successful execution
+ */
+int main(void)
+{
+	char n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+	}
+	for (n = 97; n <= 102; n++)
+	{
+		putchar(n);
+	}
+	
+	putchar('\n');
+return (0);
+}

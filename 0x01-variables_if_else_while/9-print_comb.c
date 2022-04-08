0dd1a8cdb8717394ee40653c5a5fb8c409c5041a
biruk <you@example.com>
2022-04-08 15:49:34 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,11 +8,11 @@ int main(void)
 {
 	int n;
 
-	for(n = 48; n <= 57, n++)
+	for(n = 48; n <= 57; n++)
 	{
 		putchar(n);
-		putchar(,);
-		putchar( );
+		putchar(',');
+		putchar(' ');
 	}
 
 	putchar('\n');

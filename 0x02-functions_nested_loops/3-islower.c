659b4dee0dcfc213f19c46c79b50a03c7a214584
biruk <you@example.com>
2022-04-12 12:14:08 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _islower - returns 1 for lowercase alphabet, other wise returns 0
+ *
+ * Return: return 1 for lower case: return 0 for uppercase
+ */
+int _islower(int c)
+	{
+		if (c >= 'a' && c <= 'z')
+		{
+			return (1);
+		}
+		else
+		{
+			return (0);
+		}
+	}

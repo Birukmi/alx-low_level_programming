30b1ff347bcbb4283058311b8b90118273373eb7
biruk <you@example.com>
2022-04-12 11:35:13 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - entry point
+ * print_alphapet - prints alphabet in lower case
+ *
+ * REturn: return value
+ * */
+int main(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	
+	_putchar('\n');
+	return (0);

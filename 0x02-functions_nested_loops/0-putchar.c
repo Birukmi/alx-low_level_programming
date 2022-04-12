89877cad52a61f05ccb98c929ea8aa5e05a112a4
biruk <you@example.com>
2022-04-12 11:15:10 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	cahr a[]= "_putchar";
+
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putcha(a[]);
+	}
+
+	_putchar("\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+/**
+ * _putchar - writes the character c in stdout
+ * @c: the character to print
+ *
+ * Return:success on 1
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}	

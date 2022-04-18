a7c110afe9350a62b26ba7d1b69f2a5ab67bec5b
biruk <you@example.com>
2022-04-18 00:47:06 -0700

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+nclude "main.h"
+/**
+ * reset_to_98 - updates the value it points to to 98
+ *
+ * @n: A pointer to an int that will be updated
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}

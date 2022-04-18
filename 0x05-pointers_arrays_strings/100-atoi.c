95261dad7b8d0fb86303fb45a5d0dd89251210a7
biruk <you@example.com>
2022-04-18 03:41:06 -0700

    atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -3,15 +3,16 @@
  * is_numerical - check if it is a digit
  * @n: Number
  * Return: If is a number, return 1 else return 0
- * _atoi - convert a string to an integer.
- * @s: string
- *
- * Return: always 0
  */
 int is_numerical(unsigned int n)
 {
 	return (n >= '0' &&  n <= '9');
 }
+/**
+ * _atoi - convert a string to an integer.
+ * @s: string
+ * Return: always 0
+ */
 int _atoi(char *s)
 {
 	unsigned int num, i;

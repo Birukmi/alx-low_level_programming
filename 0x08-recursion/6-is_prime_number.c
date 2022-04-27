883ebe9d5ab7a54b48b46c4e2d3ac2fceeecb515
biruk <you@example.com>
2022-04-27 14:51:51 -0700

    prime

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,43 @@
+#include "main.h"
+/**
+ * is_prime_number - evaluate prime or not
+ * @n: integer no
+ * Return: reteurn the value of the function
+ */
+int is_prime_number(int num)
+{
+	int i;
+
+	i = 2;
+	if (num < 2)
+	{
+		return (0);
+	}
+	if (num == 2)
+	{
+		return (1);
+	}
+	return (evaluate(num, i));
+}
+/**
+ * evaluate - evaluate number
+ * @num: integer value
+ * @i: integer
+ * Return: return evaluate function value
+ */
+int evaluate(num, i)
+{
+	if (i == num - 1)
+	{
+		return (1);
+	}
+	else if (num % i == 0)
+	{
+		return (0);
+	}
+	if (num % i != 0)
+	{
+		return (evaluate(num, i + 1));
+	}
+	return (0);
+}

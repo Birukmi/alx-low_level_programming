3761d827b26f3b60cf2a7372fa7db30874c03b07
biruk <you@example.com>
2022-04-27 15:29:53 -0700

    prme

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,43 @@
+#include "main.h"
+/**
+ * is_palindrome - checks if a string is a palindrome
+ * @s: a pinter string
+ * Return: 0 or 1
+ */
+int is_palindrome(char *s)
+{
+	int len = _strlen_recursion(s);
+
+	return (pali(s, len - 1))
+}
+/**
+ * _strlen_recursion - length
+ * @s: pointer string
+ * Return: return strlen function value
+ */
+int _strlen_recursion(char *s)
+{
+	if (!*s)
+	{
+		return (0);
+	}
+	return (1 + _strlen_recursion(++s));
+}
+/**
+ * pali - checking palindrom
+ * @s: string pointer
+ * @i: place
+ * Return: value
+ */
+int pali(char *s, int i)
+{
+	if (i < 1)
+	{
+		return (1);
+	}
+	if (*s == *(s + i))
+	{
+		return (pali(s + 1, i - 2));
+	}
+	return (0);
+}

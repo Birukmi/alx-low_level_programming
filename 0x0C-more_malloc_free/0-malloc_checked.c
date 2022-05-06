1f0c9aac5b8ee4d56c8ed1c26fc11255a6da7d7d
biruk <you@example.com>
2022-05-06 15:25:25 -0700

    mallocChecked

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * malloc_checked - allocates memory 
+ * @b: number of memory
+ * Return: void
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *i;
+
+	i = malloc(b);
+	if (i == NULL)
+	{
+		exit(98);
+	}
+	return (i);
+}

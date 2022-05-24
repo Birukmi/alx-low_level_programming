75fa8a873681610aca4eb41a031a3071572087e4
biruk <you@example.com>
2022-05-24 13:40:47 -0700

    reverselistint

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+/**
+ * print_listint_safe - prints a linked list
+ * @head: list of type listint_t to print
+ * Return: number of nodes in the list
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t m = 0;
+	long int i;
+
+	while (head)
+	{
+		i = head - head->next;
+		m++;
+		printf("[%p] %d\n", (void *)head, head->n);
+		if (i > 0)
+		{
+			head = head->next;
+		}
+		else
+		{
+			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
+			break;
+		}
+	}
+	return (m);
+}

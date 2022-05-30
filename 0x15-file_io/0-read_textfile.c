da6e37c1038636a0dae160af9361c80850c10873
biruk <you@example.com>
2022-05-30 13:35:51 -0700

    readtext

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -22,10 +22,10 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	file = open(filename, O_RDONLY);
 	if (file == -1)
 		return (0);
-	fread = read(file, totalSize, letters);
+	fread = read(file, total, letters);
 	if (fread == -1)
 		return (0);
-	fwrite = write(STDOUT_FILENO, totalSize, fread);
+	fwrite = write(STDOUT_FILENO, total, fread);
 	if (fwrite == -1)
 	{
 		return (0);
